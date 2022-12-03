5b0bd58dec2b113e63a2ffe183c4403a8048b765
RcINS <rcins@duck.com>
2022-12-04 01:36:04 +0800

    mod

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 from src import PinterestScraper, PinterestConfig
 
 
-configs = PinterestConfig(search_keywords="Atatürk", # Search word
-                          file_lengths=200,     # total number of images to download (default = "100")
+configs = PinterestConfig(search_keywords="leopard 2a6", # Search word
+                          file_lengths=2000,     # total number of images to download (default = "100")
                           image_quality="orig", # image quality (default = "orig")
                           bookmarks="")         # next page data (default= "")
 
-
-PinterestScraper(configs).download_images()     # download images directly
-print(PinterestScraper(configs).get_urls())     # just bring image links
+PinterestScraper(configs).download_images(r'E:\leopard')     # download images directly
+#print(PinterestScraper(configs).get_urls())     # just bring image links
--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,6 @@
+import time
+
+import aria2p
 import requests
 import json
 import os
@@ -7,58 +10,78 @@ import sys
 
 class Scraper:
 
-    def __init__(self, config, image_urls=[]):
+    def __init__(self, config):
         self.config = config
-        self.image_urls = image_urls
+
+        self.aria2 = aria2p.API(aria2p.Client(
+            host="http://127.0.0.1",
+            port=6800,
+            secret=""
+        ))
 
     # Set config for bookmarks (next page)
     def setConfig(self, config):
         self.config = config
 
     # Download images
-    def download_images(self):
-        folder = "photos/"+self.config.search_keyword.replace(" ", "-")
-        number = 0
+    def download_images(self, save_dir):
         # prev get links
         results = self.get_urls()
+
         try:
-            os.makedirs(folder)
-            print("Directory ", folder, " Created ")
+            os.makedirs(save_dir)
+            print("Directory ", save_dir, " Created ")
         except FileExistsError:
-            a = 1
-        arr = os.listdir(folder+"/")
-        for i in results:
-            if str(i + ".jpg") not in arr:
-                try:
-                    file_name = str(i.split("/")[len(i.split("/"))-1])
-                    download_folder = str(folder) + "/" + file_name
-                    print("Download ::: ", i)
-                    urllib.request.urlretrieve(i,  download_folder)
-                    number = number + 1
-                except Exception as e:
-                    print(e)
+            pass
+
+        downloads = []
+
+        for url in results:
+            try:
+                print("Add ::: ", url)
+
+                options = {'dir': save_dir, 'out': url.split("/")[-1], 'continue': True,
+                           'max-tries': 5, 'auto-file-renaming': False,
+                           'split': 1}
+                downloads.append(self.aria2.add_uris([url], options))
+            except Exception as e:
+                print(e)
+
+        while any(downloads):
+            download = downloads.pop(0)
+            while True:
+                download.update()
+                if download.has_failed:
+                    self.aria2.remove([download])
+                    downloads.append(self.aria2.add_uris([download.files[0].uris[0].values()[0]], download.options))
+                elif download.is_complete:
+                    break
+                else:
+                    time.sleep(1)
 
     # get_urls return array
     def get_urls(self):
-        SOURCE_URL = self.config.source_url,
-        DATA = self.config.image_data,
-        URL_CONSTANT = self.config.search_url
-        r = requests.get(URL_CONSTANT, params={
-                         "source_url": SOURCE_URL, "data": DATA})
-        jsonData = json.loads(r.content)
-        resource_response = jsonData["resource_response"]
-        data = resource_response["data"]
-        results = data["results"]
-        for i in results:
-            self.image_urls.append(
-                i["images"][self.config.image_quality]["url"])
+        count = 0
+        while count < int(self.config.file_length):
+            SOURCE_URL = self.config.source_url,
+            DATA = self.config.image_data,
+            URL_CONSTANT = self.config.search_url
+            r = requests.get(URL_CONSTANT, params={
+                             "source_url": SOURCE_URL, "data": DATA})
+            jsonData = json.loads(r.content)
+            resource_response = jsonData["resource_response"]
+            data = resource_response["data"]
+            results = data["results"]
+            for i in results:
+                count += 1
+                yield i["images"][self.config.image_quality]["url"]
 
-        if len(self.image_urls) < int(self.config.file_length):
-            self.config.bookmarks = resource_response["bookmark"]
-            # print(self.image_urls)
-            print("Creating links", len(self.image_urls))
-            self.get_urls()
-            return self.image_urls[0:self.config.file_length]
+            try:
+                self.config.bookmarks = resource_response["bookmark"]
+            except:
+                return
+            finally:
+                print("Creating links", count)
 
         #if len(str(resource_response["bookmark"])) > 1 : connect(query_string, bookmarks=resource_response["bookmark"])
 
